b31b2702df9e89c51fb4beb2e13d441d5a10f189
Carlson <olibadua@gmail.com>
2025-03-31 08:15:56 +0800

    Added PizzaBrowser(Razor, cs file)

---
+++ Client/PizzaSpecials.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBasic
+{
+    public class PizzaSpecials
+    {
+
+        private int PizzaID { get; set; }
+        private string PizzaName { get; set; }
+        private decimal PizzaBasePrice { get; set; }
+        private string PizzaDescription { get; set;}
+        private string PizzaImageUrl { get; set; }
+        public string GetFormattedBasePrice() => PizzaBasePrice.ToString("0.00");
+    }
+}
