26de158e25670ba15806752d5c1e9f1423ca5bfa
Carlson <olibadua@gmail.com>
2025-03-31 16:19:11 +0800

    uploading for the mean time. Assembly is different for current exercise

---
+++ Client/Data/PizzaStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BlazorBasic.Data
+{
+    public class PizzaStoreContext : DbContext
+    {
+        public PizzaStoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<PizzaSpecial> Specials { get; set; }
+    }
+}
