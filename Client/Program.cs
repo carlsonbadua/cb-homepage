787453ae58c81621a6095b3f80e41e567e034514
Carlson <olibadua@gmail.com>
2025-04-06 12:42:50 +0800

    Returned to basic with the project. Will be performing new csproj on new project file

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,14 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using BlazorBasic;
-using BlazorBasic.PizzaProject.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 var app = builder.Build();
@@ -17,12 +15,3 @@ var app = builder.Build();
 
 await builder.Build().RunAsync();
 
-var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
-using (var scope = scopeFactory.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
-    if (db.Database.EnsureCreated())
-    {
-        SeedData.Initialize(db);
-    }
-}
